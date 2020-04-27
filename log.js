d863e856dc394dd540a2820f8f9d554406efdb4e
naieemsupto <naieem.mahmud@selise.ch>
2020-04-27 15:06:45 +0600

    per execution new log file generation code done

--- log.js
+++ log.js
@@ -1,10 +1,18 @@
 // create a custom timestamp format for log statements
 var Logger = require('simple-node-logger'); // importing node logger
-// logger options
-var logOptions = {
-    logFilePath: 'logfile.log',
-    timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS'
-};
-var log = Logger.createSimpleLogger(logOptions);
-log.setLevel('info'); // setting default level for logging
-module.exports = log;
+
+function initialize() {
+    var logDate = new Date();
+    var logFileName = "_" + logDate.getDate() + "_" + (logDate.getMonth() + 1) + "_" + logDate.getHours() + "_" + logDate.getMinutes();
+    // logger options
+    var logOptions = {
+        logFilePath: 'logfile' + logFileName + '.log',
+        timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS'
+    };
+    var log = Logger.createSimpleLogger(logOptions);
+    log.setLevel('info'); // setting default level for logging
+    return log;
+}
+module.exports = {
+    initialize: initialize
+}
