c2d7b17bfe5046d088ccc2ddfc3642778c339e6b
naieemsupto <naieem.mahmud@selise.ch>
2020-05-08 16:30:41 +0600

    dbwise backup initialized

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     ExportDBConnectionString: "10.5.3.21:27017",
+    DBNames:['A154AE88-1965-43EE-A9B8-2028F2493E74'], // need for exporting
     ImportDBConnectionString: "10.5.3.67:27017",
     ExportStoreFolder: "./dump/dump_10_5_3_21_backup_from_19_4_12_00",
     RabbitConfig: {
@@ -9,6 +10,6 @@ module.exports = {
         hostname: "172.16.0.160"
     },
     RabbitQueueName: "supto-test",
-    IsADateQuery: true,
+    IsADateQuery: false,
     FromDateOfDateQuery: "2020-04-19T12:00:00.000Z"
 }
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const config = require("./config");
 const log = require('./log');
 const mongoose = require('mongoose')
     , Admin = mongoose.mongo.Admin;
-const connectionString = "mongodb://10.5.3.67:27017,10.5.3.239:27017,10.5.3.227:27017/?replicaSet=rs0&readPreference=primaryPreferred";
+// const connectionString = "mongodb://10.5.3.67:27017,10.5.3.239:27017,10.5.3.227:27017/?replicaSet=rs0&readPreference=primaryPreferred";
+// const connectionString = "10.5.3.21:27017";
 console.log(process.argv[2]);
 // var connection = mongoose.createConnection(connectionString, { useNewUrlParser: true });
 // connection.once('open', () => {
@@ -13,24 +14,34 @@ console.log(process.argv[2]);
 rabbit.rabbitInit().then((response) => {
     if (response) {
         /// create a connection to the DB    
-        var connection = mongoose.createConnection('mongodb://' + connectionString, { useNewUrlParser: true });
+        var connection = mongoose.createConnection('mongodb://' + config.ExportDBConnectionString, { useNewUrlParser: true });
         connection.once('open', () => {
-            // connection established
-            new Admin(connection.db).listDatabases((err, result) => {
-                // console.log('Total DB Found ', result.databases.length);
-                log.info('Total DB Found ' + result.databases.length);
-                // database list stored in result.databases
-                var allDatabases = result.databases;
-                allDatabases.forEach(database => {
-                    // console.log(database.name);
+            log.info("connection created");
+            if (config.DBNames.length) {
+                config.DBNames.forEach(database => {
                     rabbit.sendToQueue({
-                        db: database.name,
-                        connectionString: connectionString
+                        db: database,
+                        connectionString: config.ExportDBConnectionString
                     });
                 });
-                connection.close();
-                rabbit.consumerInit(process.argv[2]);
-            });
+            } else {
+                // connection established
+                new Admin(connection.db).listDatabases((err, result) => {
+                    // console.log('Total DB Found ', result.databases.length);
+                    log.info('Total DB Found ' + result.databases.length);
+                    // database list stored in result.databases
+                    var allDatabases = result.databases;
+                    allDatabases.forEach(database => {
+                        // console.log(database.name);
+                        rabbit.sendToQueue({
+                            db: database.name,
+                            connectionString: connectionString
+                        });
+                    });
+                });
+            }
+            connection.close();
+            rabbit.consumerInit(process.argv[2]);
         });
     }
 });
